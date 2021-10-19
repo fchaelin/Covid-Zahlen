70eefdf92cd3ffdf0cd2925e3055242ec4842283
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2021-10-19 11:30:52 +0200

    für html gemacht

--- functions_covid.php
+++ functions_covid.php
@@ -1,28 +1,22 @@
 <?php
 
-function landZahlen()
+function writeFile($filename, $total, $totaldeaths)
 {
-    $url = "https://covid2019-api.herokuapp.com/v2/country/";
-    $country = readline("Land: ");
-    switch ($country) {
-        case "usa":
-            $country = "us";
-            break;
-        case "america":
-            $country = "us";
-            break;
-        case "united states of america":
-            $country = "us";
-            break;
-        case "ch":
-            $country = "switzerland";
-            break;
-        default:
-            $country = $country;
-    }
+    $template = file_get_contents("template.html");
+    $template = str_replace("{Section1}", "<h2>Confirmed: " . $total . "</h2>", $template);
+    // $template = str_replace("{Section1.Content}", $total, $template);
+    $template = str_replace("{Section2.Title}", "<h2>Deaths</h2>", $template);
+    $template = str_replace("{Section2.Content}", $totaldeaths, $template);
+    $template = str_replace("{Section3.Title}", "", $template);
+    $template = str_replace("{Section3.Content}", "", $template);
+    // echo $template;
+    $myfile = fopen(htmls/$filename, "w") or die("Unable to open file!");
+    fwrite($myfile, $template);
+    fclose($myfile);
+}
 
-    $url .= $country;
-
+function loadData($url)
+{
     $headers = array(
         'Accept: application/json',
         'Content-Type: application/json',
@@ -39,27 +33,71 @@ function landZahlen()
     $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
     curl_close($curl);
 
-
     if ($code == 200) {
         $response = json_decode($response, true);
-        //print_r($response);
-        print PHP_EOL;
-
-        $confirmed = $response['data']['confirmed'];
-        $deaths = $response['data']['deaths'];
-        //$recovered = $response['data']['recovered'];
-        //$active = $response['data']['active'];
-
-        printf("Confirmed: %d" . PHP_EOL, $confirmed);
-        printf("Deaths: %d" . PHP_EOL, $deaths);
-        //printf("Recovered: %d" . PHP_EOL, $recovered);
-        //printf("Active: %d" . PHP_EOL, $active);
+    } else {
+        $response = "fail";
     }
+    return $response;
+}
+function landZahlen($land)
+{
+    $url = "https://covid2019-api.herokuapp.com/v2/country/";
+    if (!empty($land)) {
+        $country = $land;
+    } else {
+        $country = readline("Land: ");
+        switch ($country) {
+            case "usa":
+                $country = "us";
+                break;
+            case "america":
+                $country = "us";
+                break;
+            case "united states of america":
+                $country = "us";
+                break;
+            case "ch":
+                $country = "switzerland";
+                break;
+            default:
+                $country = $country;
+        }
+    }
+    $url .= $country;
+    $response = loadData($url);
+    //print_r($response);
+    print PHP_EOL;
+
+    $confirmed = $response['data']['confirmed'];
+    $deaths = $response['data']['deaths'];
+    //$recovered = $response['data']['recovered'];
+    //$active = $response['data']['active'];
+
+    printf("Confirmed: %d" . PHP_EOL, $confirmed);
+    printf("Deaths: %d" . PHP_EOL, $deaths);
+    //printf("Recovered: %d" . PHP_EOL, $recovered);
+    //printf("Active: %d" . PHP_EOL, $active);
+    writeFile($country . ".html", $confirmed, $deaths);
 }
 
 function globaleZahlen()
 {
     $url = "https://covid2019-api.herokuapp.com/v2/total";
+    $response = loadData($url);
+    $total = $response['data']['confirmed'];
+    $totaldeaths = $response['data']['deaths'];
+    printf("Total confirmed: %d" . PHP_EOL, $total);
+    printf("Total deaths: %d" . PHP_EOL, $totaldeaths);
+    writeFile("global.html", $total, $totaldeaths);
+}
+
+function vergangeneZahlen()
+{
+
+    $requestedDate = readline("Datum (: ");
+
+    $url = "https://covid2019-api.herokuapp.com/v2/timeseries/confirmed";
 
     $headers = array(
         'Accept: application/json',
@@ -82,14 +120,8 @@ function globaleZahlen()
         //print_r($response);
         print PHP_EOL;
 
-        $total = $response['data']['confirmed'];
-        $totaldeaths = $response['data']['deaths'];
-        //$totalrecovered = $response['data']['recovered'];
-        //$totalactive = $response['data']['active'];
 
-        printf("Total confirmed: %d" . PHP_EOL, $total);
-        printf("Total deaths: %d" . PHP_EOL, $totaldeaths);
-        //printf("Total recovered: %d" . PHP_EOL, $totalrecovered);
-        //printf("Total active: %d" . PHP_EOL, $totalactive);
     }
+
+
 }
