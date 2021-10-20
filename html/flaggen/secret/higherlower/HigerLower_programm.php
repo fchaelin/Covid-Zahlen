ad8a18fa0cc9f4847a3e0709a7d80e0708939240
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2021-10-20 15:04:57 +0200

    Secret hinzugefügT

--- html/flaggen/secret/higherlower/HigerLower_programm.php
+++ html/flaggen/secret/higherlower/HigerLower_programm.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "asdf";
+echo "Hier musst du raten welches Land mehr Coronafälle hat";
+PHP_EOL;
+$ready = readline("Bereit? (j/n) > ");
+
+if ($ready == "j" or "J"){
+    include('functions_HigherLower.php');
+    system('clear');
+    whichIsHigherFrage();
+} elseif ($ready == "n" or "N"){
+    PHP_EOL;
+    echo "Ok";
+} else {
+    echo "ungültige eingabe";
+}
--- html/flaggen/secret/secret_programm.php
+++ html/flaggen/secret/secret_programm.php
@@ -1,37 +1,44 @@
 <?php
 
+function secret()
+{
 
 
-print PHP_EOL;
 
-echo 'Themenauswahl';
-print PHP_EOL;
-echo '*************';
-print PHP_EOL;
-echo '1 Upper-Lower Covid';
-print PHP_EOL;
-echo '2 Flaggenquiz';
-print PHP_EOL;
-echo '3 Denis Secret';
-print PHP_EOL;
+    print PHP_EOL;
+
+    echo 'Secretauswahl';
+    print PHP_EOL;
+    echo '*************';
+    print PHP_EOL;
+    echo '1 Higher-Lower Covid';
+    print PHP_EOL;
+    echo '2 Flaggen zu Länder';
+    print PHP_EOL;
+    echo '3 Denis Secret';
+    print PHP_EOL;
+
+    chdir("/opt/code/covid/html/flaggen/secret/");
 
 
-$auswahl = readline('Auswahl: ');
+    $auswahl = readline('Auswahl: ');
 
-switch ($auswahl) {
-    case 1:
-        echo "Sie haben das \"Upper-Lower Covid\" gewählt!";
-        include ("higherlower/HigherLower_programm.php");
-        break;
-    case 2:
-        echo "Sie haben das \"Flaggenquiz\" gewählt!";
-        break;
-    case 3:
-        echo "Sie haben das \"Denis Secret\" gewählt!";
-        break;
-    default:
-        echo "Dieses Modul gibt es nicht!";
+    switch ($auswahl) {
+        case 1:
+            echo "Sie haben das \"Upper-Lower Covid\" gewählt!";
+            include ("higherlower/HigerLower_programm.php");
+            break;
+        case 2:
+            echo "Sie haben das \"Flaggen zu Länder\" gewählt!";
+            include ("flaggenzuland/index.php");
+            break;
+        case 3:
+            echo "Sie haben das \"Denis Secret\" gewählt!";
+            break;
+        default:
+            echo "Dieses Secret gibt es nicht!";
+    }
+
+    print PHP_EOL;
+    print PHP_EOL;
 }
-
-print PHP_EOL;
-print PHP_EOL;
