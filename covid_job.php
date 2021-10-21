9d01f4ea9d07e60bf7106cae93e0a68470f00baf
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2021-10-21 14:33:18 +0200

    secret entfernt

--- covid_job.php
+++ covid_job.php
@@ -4,21 +4,7 @@ chdir("/opt/code/covid/");
 
 include("functions_covid.php");
 
-if (!empty($argv[1])) {
-    if ($argv[1] == 0) {
-        $eingabe = $argv[1];
-    } elseif ($argv[1] == 1) {
-        $eingabe = $argv[1];
-    } elseif ($argv[1] == 2) {
-        $eingabe = $argv[1];
-    } elseif ($argv[1] == 3) {
-        $eingabe = $argv[1];
-    } elseif ($argv[1] == 1234567890) {
-        $eingabe = $argv[1];
-    } else {
-        $eingabe = readline("Welches Programm 0-3: ");
-    }
-} else {
+if (empty($argv[1])) {
     print PHP_EOL;
 
     echo 'Programmauswahl';
@@ -31,11 +17,43 @@ if (!empty($argv[1])) {
     print PHP_EOL;
     echo '2 Länder-Datumszahlen';
     print PHP_EOL;
+    echo '3 Globale-Datumszahlen';
+    print PHP_EOL;
 
     $eingabe = readline('Auswahl: ');
 
     print PHP_EOL;
     print PHP_EOL;
+} else {
+    if ($argv[1] == 0) {
+        $eingabe = $argv[1];
+    } elseif ($argv[1] == 1) {
+        $eingabe = $argv[1];
+    } elseif ($argv[1] == 2) {
+        $eingabe = $argv[1];
+    } elseif ($argv[1] == 3) {
+        $eingabe = $argv[1];
+    } else {
+        print PHP_EOL;
+
+        echo 'Programmauswahl:';
+        print PHP_EOL;
+        echo '***************';
+        print PHP_EOL;
+        echo '0 Länder-Gesamtzahlen';
+        print PHP_EOL;
+        echo '1 Global-Zahlen';
+        print PHP_EOL;
+        echo '2 Länder-Datumszahlen';
+        print PHP_EOL;
+        echo '3 Globale-Datumszahlen';
+        print PHP_EOL;
+
+        $eingabe = readline('Auswahl: ');
+
+        print PHP_EOL;
+        print PHP_EOL;
+    }
 }
 
 
@@ -66,12 +84,12 @@ switch ($eingabe) {
         landDatumZahlen($land, $requestedDate);
         break;
     case '3':
-        //globalDatumZahlen();
+        if (!empty($argv[2])) {
+            $requestedDate = $argv[2];
+        } else {
+            $requestedDate = readline("Datum m(m)/d(d)/yy: ");
+        }
+        globalDatumZahlen($requestedDate);
         break;
-    case '1234567890':
-        include("html/flaggen/secret/secret_programm.php");
-        secret();
-
-
     default:
 }
